89a7516af00b68b24d066fa7afc5f6a765e1d54c
“kishlaychandan00” <kishlaychandan00@email.com>
2024-05-21 18:41:18 +0530

    adding DOM project 1

---
+++ index.js
@@ -0,0 +1,8 @@
+let text=document.getElementById("main");
+text.style.backgroundColor='yellow';
+text.style.margin='20px';
+text.style.padding='10px';
+text.style.fontSize='18px'
+text.style.fontWeight='bold';
+text.style.height='200px'
+text.style.width='300px'
